c25d29037519f687b5079a887860b5fdf0bed1aa
Pedro Ferrette <pedrovf@live.com>
2022-10-18 19:00:13 -0300

    primeiro código

---
+++ Inventario Steris/Inventario Steris/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Inventario_Steris.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
