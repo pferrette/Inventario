1cc7db6f73de21cd26d687a0270cb8a3efe06f98
Pedro Ferrette <pedrovf@live.com>
2022-10-18 22:01:41 -0300

    Adicionado o Entity e a criação de alguns UML para ajudar

---
+++ Inventario Steris/Inventario Steris/Models/Computador.cs
@@ -0,0 +1,17 @@
+﻿namespace Inventario_Steris.Models
+{
+    public class Computador
+    {
+        public int ComputadorId { get; set; }
+        public string ComputadorName { get; set; }
+        public string ServiceTag { get; set; }
+        public string SterisTag { get; set; }
+        public string SerconTag { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string ExpressCode { get; set; }
+        public DateTime DataUltimaTroca { get; set; }
+        public string MotivoTroca { get; set; }
+
+    }
+}
---
+++ Inventario Steris/Inventario Steris/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿namespace Inventario_Steris.Models
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Nome { get; set; }
+        public int CentroDeCusto { get; set; }
+        public string Supervisor { get; set; }
+        public string Email { get; set; }
+        public bool SignatureEquip { get; set; }
+        public bool SignatureDados { get; set; }
+
+        public List<Celular> Celulares { get; set; }
+        public List<Computador> Computadores { get; set; }
+
+
+    }
+}
