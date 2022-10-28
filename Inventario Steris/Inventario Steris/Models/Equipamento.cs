dc8d03d56f50c76f4ab6b5955756f73a40c37939
Pedro Ferrette <pedrovf@live.com>
2022-10-28 18:57:28 -0300

    Fazendo conexão com o Banco de dados via Entity. Adicionando Classe Equipamento

---
+++ Inventario Steris/Inventario Steris/Models/Equipamento.cs
@@ -0,0 +1,21 @@
+﻿namespace Inventario_Steris.Models
+{
+    public class Equipamento
+    {
+        public int EquipamentoId { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string SterisTag { get; set; }
+        public string SerconTag { get; set; }
+        public string Local { get; set; }
+        public string Situação { get; set; }
+        public string Detalhes { get; set; }
+        public string Tipo { get; set; }
+        public int QtdUsuarios { get; set; }
+        public List<Computador> Computadores { get; set; }
+        public List<Celular> Celulares { get; set; }
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+
+    }
+}
